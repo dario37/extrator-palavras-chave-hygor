f6610a45397f1fa60aea34d08bf0893b70fefc3d
dario37 <hygor.dario@escola.pr.gov.br>
2025-10-28 10:42:22 -0300

    28/10

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ function mostraPalavrasChave() {
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
     const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
 
 
 
