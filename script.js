68711551204eeed979ccd20fb1ea95a00b7cb734
dario37 <hygor.dario@escola.pr.gov.br>
2025-10-28 09:42:45 -0300

    28/10

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
-function mostraPalavrasChave(){
+function mostraPalavrasChave() {
     const texto = document.querySelector("#entrada-de-texto").value;
     const campoResultado = document.querySelector("#resultado-palavrachave");
-    const palavrasChave= processaTexto(texto);
+    const palavrasChave = processaTexto(texto);
 
-    campoResultado. textContent = palavrasChave.join(", ")
+    campoResultado.textContent = palavrasChave.join(", ")
 }
 
-function processaTexto(texto){
-let palavras = texto.split(/\p{L}+/u);
+function processaTexto(texto) {
+    let palavras = texto.split(/\p{L}+/u);
 
-return palavras;
+    return palavras;
 
 
 }
