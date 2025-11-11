47d0d8671aa38e0d3ed63a466d9852d8b5b300e5
dario37 <hygor.dario@escola.pr.gov.br>
2025-11-11 09:24:38 -0300

    11/11

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import { PALAVRAS_RUINS } from"./palavrasRuins";
+
+
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
